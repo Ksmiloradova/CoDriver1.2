1796d1d516ae7856e0af986a6d3c3ff639e58ae5
Ksmiloradova <ksmiloradova@edu.hse.ru>
2022-07-11 11:14:54 +0300

    Initial commit

---
+++ app/src/main/java/ru/team2/codriver12/ContactsActivity.kt
@@ -0,0 +1,19 @@
+package ru.team2.codriver12
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Contacts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts)
+        var webText: TextView = findViewById(R.id.textView8);
+        webText.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.consultant.ru/"))
+            startActivity(i)
+        }
+    }
+}
